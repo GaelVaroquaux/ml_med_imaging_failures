faebf6fcf6ab5e3f9e7eddb361781836b9d12b8d
Gael Varoquaux <gael.varoquaux@normalesup.org>
2021-03-19 22:44:19 +0100

    fig

--- plot_publicationgap.py
+++ plot_publicationgap.py
@@ -26,18 +26,22 @@ df_counts['breast_vs_all_medical'] = df_counts['breast_medical'] / df_counts['al
 df_counts['breast_vs_all_ai'] = df_counts['breast_ai'] / df_counts['all_ai'] * 100
 
 
-keys = ['lung_vs_all_medical','lung_vs_all_ai', 'breast_vs_all_medical', 'breast_vs_all_ai']
-
-labels = ['lung, medical', 'lung, AI', 'breast, medical', 'breast, AI']
-
 plt.figure(figsize=(3.8, 3))
 
-plt.axvline(2016, color='.3', linestyle='--')
-plt.text(2016.1, 4.9, 'Kaggle lung\nchallenge', rotation=90, va='top')
+plt.axvline(2016, color='.3', linestyle='-')
+plt.text(2016.1, 4.95, 'Kaggle lung\n  challenge', rotation=90, va='top',
+         size=13)
 
 
-for key, label in zip(keys, label):
-    plt.plot(df_counts['year'], df_counts[key])
+plt.plot(df_counts['year'], df_counts['lung_vs_all_medical'],
+         label='lung, medical', color='C0', linestyle='-')
+plt.plot(df_counts['year'], df_counts['lung_vs_all_ai'],
+         label='lung, AI', color='C0', linestyle='--')
+
+plt.plot(df_counts['year'], df_counts['breast_vs_all_medical'],
+         label='breast, medical', color='C1', linestyle='-')
+plt.plot(df_counts['year'], df_counts['breast_vs_all_ai'],
+         label='breast, AI', color='C1', linestyle='--')
 
 
 plt.ylim(1, 5)
@@ -47,12 +51,11 @@ plt.ylabel("Fraction of papers in category       ", size=14)
 plt.xlabel("Publication year", size=14)
 plt.yticks((1, 2, 3, 4, 5), ('1%', '2%', '3%', '4%', '5%'))
 
-plt.legend(,
-            loc='upper left',
-            title="Topic and field\n of the publication",
-            #frameon=False,
-            borderaxespad=.2,
-            )
+plt.legend(loc='upper left',
+           title="Topic and field\n of the publication",
+           #frameon=False,
+           borderaxespad=.2,
+           )
 
 plt.tight_layout(pad=.01)
 plt.savefig('figures/publication_counts.pdf', transparent=True)
