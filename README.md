4fcee463659f43b6b141b0c63646f3ea09a3975d
Gael Varoquaux <gael.varoquaux@normalesup.org>
2021-03-19 17:47:29 +0100

    iter
    Clean up

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-# failed_machine_learning
+# Analyses for Learning from failure - guidelines for improving machine learning in medical imaging by Varoquaux & Cheplygina
+
+
+In this repository you can find the data and code to reproduce Figures 1 to 3 from the paper.
+
+## Fig. 1 - Progress of Alzheimer's diagnosis challenges
 
 * paper_tables_raw - tables as saved from HTML, or extracted by Tabula from the paper PDF.
-* paper_tables_clean - tables, after cleaning by hand 
-* segmentation_metrics have scores from challenges, provided to others by Veronika. We must ask permission before using these and acknowledge relevant people. 
+* paper_tables_clean - same tables after cleaning by the raw tables  
+* plot_papers.py - code creating the figure
+
+## Fig. 2 - Lung vs breast cancer in medical vs medical machine learning papers 
+
+* publications - Excel file with publication numbers extracted from Dimensions.AI
+* plot_publicationgap.py - code creating the figure
+
+## Fig. 3 - Analysis of gaps between public and private leaderboards on Kaggle
+
+* kaggle - tables with the raw data about the challenges
+* plot_kaggle.py - code creating the figure
---
+++ plot_publicationgap.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Plot the publication counts on a few topics
+"""
+from matplotlib import pyplot as plt
+import matplotlib.lines as mlines
+import pandas as pd
+import numpy as np
+import os
+import re
+
+
+
+data_path = 'publication_counts'
+
+
+df_counts = pd.read_excel(os.path.join(data_path,'publication_counts.xlsx'))
+
+
+
+df_counts['lung_vs_all_medical'] = df_counts['lung_medical'] / df_counts['all_medical'] * 100
+df_counts['lung_vs_all_ai'] = df_counts['lung_ai'] / df_counts['all_ai'] * 100
+
+
+df_counts['breast_vs_all_medical'] = df_counts['breast_medical'] / df_counts['all_medical'] * 100
+df_counts['breast_vs_all_ai'] = df_counts['breast_ai'] / df_counts['all_ai'] * 100
+
+
+keys = ['lung_vs_all_medical','lung_vs_all_ai', 'breast_vs_all_medical', 'breast_vs_all_ai']
+
+labels = ['lung, medical', 'lung, AI', 'breast, medical', 'breast, AI']
+
+plt.figure(figsize=(3.8, 3))
+
+plt.axvline(2016, color='.3', linestyle='--')
+plt.text(2016.1, 4.9, 'Kaggle lung\nchallenge', rotation=90, va='top')
+
+
+for key, label in zip(keys, label):
+    plt.plot(df_counts['year'], df_counts[key])
+
+
+plt.ylim(1, 5)
+plt.xlim(2011, 2019)
+
+plt.ylabel("Fraction of papers in category       ", size=14)
+plt.xlabel("Publication year", size=14)
+plt.yticks((1, 2, 3, 4, 5), ('1%', '2%', '3%', '4%', '5%'))
+
+plt.legend(,
+            loc='upper left',
+            title="Topic and field\n of the publication",
+            #frameon=False,
+            borderaxespad=.2,
+            )
+
+plt.tight_layout(pad=.01)
+plt.savefig('figures/publication_counts.pdf', transparent=True)
